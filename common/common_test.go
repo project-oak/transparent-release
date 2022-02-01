efcfcff378427aaaf8369b995b854a2f04ad57eb
Juliette Pretot <julsh@google.com>
2022-02-01 16:59:30 +0000

    Make command a string
    It is parsd as a string, hence we'd like to change it. The SLSA amber build type uses a string as well.

--- common/common_test.go
+++ common/common_test.go
@@ -69,7 +69,7 @@ func checkBuildConfig(got *BuildConfig, t *testing.T) {
 		Repo:                     "https://github.com/project-oak/oak",
 		CommitHash:               "0f2189703c57845e09d8ab89164a4041c0af0a62",
 		BuilderImage:             "gcr.io/oak-ci/oak@sha256:53ca44b5889e2265c3ae9e542d7097b7de12ea4c6a33785da8478c7333b9a320",
-		Command:                  []string{"./scripts/runner", "build-functions-server"},
+		Command:                  "./scripts/runner build-functions-server",
 		OutputPath:               "./oak_functions/loader/bin/oak_functions_loader",
 		ExpectedBinarySha256Hash: "15dc16c42a4ac9ed77f337a4a3065a63e444c29c18c8cf69d6a6b4ae678dca5c",
 	}
