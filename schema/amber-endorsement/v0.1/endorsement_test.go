0140394e3348fe0030cdd7859e5ed81f1f60034d
Razieh Behjati <razieh@google.com>
2022-02-10 13:05:05 +0000

    Add TODO note

--- schema/amber-endorsement/v0.1/endorsement_test.go
+++ schema/amber-endorsement/v0.1/endorsement_test.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// TODO(#23): Move this to the `amber` package.
 package schema
 
 import (
