1a288284444d519168bbb7916e31686cc14502d1
Andrew Ferraiuolo <aferr@google.com>
2022-03-09 18:01:41 +0000

    Some Cleanup

--- experimental/auth-logic/run_auth_logic.go
+++ experimental/auth-logic/run_auth_logic.go
@@ -3,19 +3,17 @@ package main
 import (
   "fmt"
   "os/exec"
-  "os"
   "io/ioutil"
   "strings"
 )
 
 func process_command(cmd *exec.Cmd) {
-  stdout, err := cmd.Output()
+  _, err := cmd.Output()
 
   if err != nil {
         fmt.Println(err)
         return
   }
-  fmt.Println(stdout);
 }
 
 func emit_output_queries(output_directory_name string) {
@@ -23,7 +21,6 @@ func emit_output_queries(output_directory_name string) {
   for _, item := range items {
     filename := item.Name()
     if(strings.Contains(filename, "csv")) {
-      fmt.Println(filename)
       contents, _ := ioutil.ReadFile(output_directory_name + "/" + filename)
       query_name := strings.ReplaceAll(filename, ".csv", "")
       fmt.Printf("%s is %t\n", query_name, len(contents) > 0)
@@ -35,8 +32,6 @@ func run_auth_logic_compiler(input_filename string) {
 
   // Make directory for the .dl and .csv outputs from souffle
   out_dir := "./experimental/auth-logic/" + input_filename + "-outputs"
-  fmt.Println("trying to make directory: ")
-  fmt.Println(out_dir)
   // The -p flag only makes the directory if it does not exist
   // (if the directory exists and the flag is omitted, an error is thrown)
   process_command(exec.Command("mkdir", "-p", out_dir))
@@ -51,17 +46,5 @@ func run_auth_logic_compiler(input_filename string) {
 }
 
 func main() {
-
-  path, err := os.Getwd()
-
-  if err != nil {
-    fmt.Println(err)
-    return
-  }
-
-  fmt.Println("Working directory is:")
-  fmt.Println(path)
-
   run_auth_logic_compiler("simple.auth_logic")
-
 }
