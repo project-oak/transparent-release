fa79ffb8cf4180b4e6d00ffff94f5be317afeefd
aferr <aferr@google.com>
2022-03-28 14:53:39 +0100

    Unix Epoch Wrapper (#35)
    A wrapper for current time in unix epoch seconds

---
+++ experimental/auth-logic/unix_epoch_time_wrapper.go
@@ -0,0 +1,34 @@
+// Copyright 2022 The Project Oak Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package authlogic contains logic and tests for interfacing with the
+// authorization logic compiler
+package authlogic
+
+import (
+	"fmt"
+	"time"
+)
+
+// This file contains a wrapper that produces the current
+// time as the number of seconds since [the unix
+// epoch](https://en.wikipedia.org/wiki/Unix_time). This wrapper
+// works by running the command `date +%s` on the local machine.
+
+type UnixEpochTime struct{}
+
+func (timeWrapper UnixEpochTime) EmitStatement() UnattributedStatement {
+	epochTime := time.Now().Unix()
+	return UnattributedStatement{fmt.Sprintf("RealTimeIs(%v).", epochTime)}
+}
---
+++ experimental/auth-logic/unix_epoch_time_wrapper_test.go
@@ -0,0 +1,72 @@
+// Copyright 2022 The Project Oak Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package authlogic contains logic and tests for interfacing with the
+// authorization logic compiler
+package authlogic
+
+import (
+	"io/ioutil"
+	"regexp"
+	"strconv"
+	"testing"
+)
+
+const (
+  // This is March 24, 2022
+  pastDate = 1648146779
+  // This is January 1, 3022
+	futureDate = 33197947200
+)
+
+func (time UnixEpochTime) Identify() Principal {
+	return Principal{"UnixEpochTime"}
+}
+
+func TestUnixEpochTimeWrapper(t *testing.T) {
+	handleErr := func(err error) {
+		if err != nil {
+			t.Fatalf("test generated error %v", err)
+			panic(err)
+		}
+	}
+
+	// Write a statement from the time wrapper to a file
+	writeErr := EmitWrapperStatement(UnixEpochTime{}, "wrapped_time.auth_logic")
+	handleErr(writeErr)
+
+	// Read the contents of the file
+	fileReadBytes, readErr := ioutil.ReadFile("wrapped_time.auth_logic")
+	handleErr(readErr)
+	fileReadString := string(fileReadBytes)
+
+	timeTestRegex := regexp.MustCompile("UnixEpochTime says { RealTimeIs\\(([0-9]+)\\). }")
+	match := timeTestRegex.FindStringSubmatch(fileReadString)
+	if len(match) != 2 {
+		t.Errorf("Result of time wrapper did not have valid format. Got: %v.",
+			fileReadString)
+	}
+
+	timeValue, conversionErr := strconv.Atoi(match[1])
+	handleErr(conversionErr)
+
+	if timeValue < pastDate {
+		t.Errorf("The emitted current time %v, already happened", timeValue)
+	}
+
+	if timeValue > futureDate {
+		t.Errorf("The emitted current time %v, is far into the future", timeValue)
+	}
+
+}
