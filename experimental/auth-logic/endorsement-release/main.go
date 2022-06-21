9935360032a2066bb7d62fe0f0089e354ce1a789
Andrew Ferraiuolo <aferr@google.com>
2022-06-21 11:53:38 +0000

    Top level and example endorsement release policy

---
+++ experimental/auth-logic/endorsement-release/main.go
@@ -0,0 +1,80 @@
+// Copyright 2022 The Project Oak Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"strings"
+)
+
+// This program accepts multiple auth logic files as input by passing them
+// into an array variable. To allow a []string to be used in flag.var, it must
+// implement flag.Value, which includes String() and Set(). And to be able to
+// define these, it must be given a type name.
+type stringArray []string
+
+func (someStringArray *stringArray) String() string {
+	return strings.Join(*someStringArray, ", ")
+}
+
+func (someStringArray *stringArray) Set(value string) error {
+	*someStringArray = append(*someStringArray, value)
+	return nil
+}
+
+func main() {
+
+	var authLogicInputs stringArray
+
+	appName := flag.String("app_name", "", "set name of application to be released")
+	provenanceFilePath := flag.String("provenance", "", "set path of provenance file")
+	outputAuthLogicFilePath := flag.String("auth_logic_out", *appName+"_endorsement_release_policy.auth_logic", "set path for generated output authorization logic verification code")
+	flag.Var(&authLogicInputs, "auth_logic_inputs", "set one or more auth logic input files; to provide multiple inputs, call the parameter more than once with each input, for example, `--auth_logic_inputs file1 --auth_logic_inputs file2`")
+
+	flag.Parse()
+
+	// Part of the code for building a project using provenance
+	// files changes the working directory. This binary needs to keep
+	// the working directory as-is, so the old working directory is saved
+	// before running generateEndorsement.
+	oldWorkingDirectory, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("couldn't get working directory before verifying: %v", err)
+	}
+
+	out, err := generateEndorsement(authLogicInputs, *appName, *provenanceFilePath)
+	if err != nil {
+		log.Fatalf("couldn't generate auth logic policy for endorsement file: %v", err)
+	}
+
+	// Restore old working directory
+	err = os.Chdir(oldWorkingDirectory)
+	if err != nil {
+		log.Fatalf("couldn't restore old working directory: %v", err)
+	}
+
+	file, err := os.Create(*outputAuthLogicFilePath)
+	defer file.Close()
+	if err != nil {
+		log.Fatalf("couldn't create file for generated authorizaiton logic: %v\nThe generated auth logic was this:\n%s", err, out)
+	}
+	_, err = file.WriteString(out)
+	if err != nil {
+		log.Fatalf("couldn't write generated authorization logic to file: %v\nThe generated auth logic was this:\n%s", err, out)
+	}
+
+}
