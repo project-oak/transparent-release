6e7b4117aca5947daa4ecc8166d498119a5dd957
Razieh Behjati <razieh@google.com>
2022-06-27 18:46:12 +0100

    Add invocation to  BuildConfig

--- common/common.go
+++ common/common.go
@@ -54,6 +54,23 @@ type BuildConfig struct {
 	OutputPath string `toml:"output_path"`
 	// Expected SHA256 hash of the output binary. Could be empty.
 	ExpectedBinarySha256Hash string `toml:"expected_binary_sha256_hash"`
+	// BuilderID maps to `builder.id` in a SLSA provenance predicate.
+	BuilderID string `toml:"builderId"`
+	// Invocation maps to `invocation` in a SLSA provenance predicate.
+	Invocation Invocation `toml:"invocation"`
+}
+
+// Invocation maps to `invocation` in a SLSA predicate, and identifies the
+// event that kicked off the build.
+type Invocation struct {
+	// URI to the script or tool that triggered the build.
+	URI string `toml:"configSource.uri"`
+	// Digest is the SHA256 digest of the config source.
+	Digest string `toml:"configSource.sha256"`
+	// EntryPoint to the invoked config source. Could be an empty string
+	EntryPoint string `toml:"configSource.entryPoint"`
+	// Parameters passed to the invoked config source.
+	Parameters []string `toml:"parameters"`
 }
 
 // RepoCheckoutInfo contains info about the location of a locally checked out
