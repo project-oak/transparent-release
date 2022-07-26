357a1a0065dfe4dab9a9002c479d54ddf60a9823
Razieh Behjati <razieh@google.com>
2022-07-26 17:07:31 +0100

    Fix linting.sh script

--- scripts/linting.sh
+++ scripts/linting.sh
@@ -2,7 +2,7 @@
 
 readonly PROJECT_ROOT_DIR="$(dirname $(dirname "$0"))"
 
-lint_errors=$(golangci-lint run --config "$PROJECT_ROOT_DIR/golangci-linters.yaml"  2>&1)
+lint_errors=$(golangci-lint run --config "$PROJECT_ROOT_DIR/golangci-linters.yaml")
 
 if [[ -z "$lint_errors" ]]; then
     echo No linting errors
