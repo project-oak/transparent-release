0b24ab0aca2b66f31f3d8db6244bd1f8fea6ad06
Razieh Behjati <razieh@google.com>
2022-08-08 13:35:41 +0100

    Unify example files

--- experimental/auth-logic/wrappers/endorsement_wrapper_test.go
+++ experimental/auth-logic/wrappers/endorsement_wrapper_test.go
@@ -23,7 +23,7 @@ import (
 	"github.com/project-oak/transparent-release/internal/testutil"
 )
 
-const testEndorsementPath = "schema/amber-claim/v1/example2.json"
+const testEndorsementPath = "experimental/auth-logic/test_data/oak_endorsement.json"
 const endorsementExpectedFile = "experimental/auth-logic/test_data/endorsement_wrapper_expected.auth_logic"
 
 func TestEndorsementWrapper(t *testing.T) {
