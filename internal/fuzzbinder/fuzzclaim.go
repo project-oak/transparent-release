ab9267a4494c5abef17813036f48e83b6c2f4a0f
Marah Gamdou <45005136+MarahGamdou@users.noreply.github.com>
2022-11-21 18:32:09 +0000

    Add FuzzClaim specification definition (#151)
    * Add FuzzClaim specification definition
    
    * Fix linting
    
    * Update comments
    
    Co-authored-by: rbehjati <razieh@google.com>
    
    * Update the description of FuzzEffort
    
    Co-authored-by: rbehjati <razieh@google.com>
    
    * Update the description of Crashes
    
    Co-authored-by: rbehjati <razieh@google.com>
    
    * Update the definition of the FuzzClaimSpec
    
    Co-authored-by: rbehjati <razieh@google.com>
    
    * Remove duplication
    
    * Fix linting errors
    
    * Fix struct design
    
    * Change naming of FuzzStatsPerTarget
    
    Co-authored-by: rbehjati <razieh@google.com>
    
    Co-authored-by: rbehjati <razieh@google.com>

---
+++ internal/fuzzbinder/fuzzclaim.go
@@ -0,0 +1,113 @@
+// Copyright 2022 The Project Oak Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package fuzzbinder provides a function for generating a fuzzing claim
+// for a revision of a source code.
+package fuzzbinder
+
+// This file provides a custom `ClaimSpec` type, FuzzClaimSpec, to be used
+// for fuzzing claims within the ClaimPredicate (defined in amber package).
+// FuzzClaimSpec is intended to be used for providing the user with the
+// needed elements to characterize the security of a revision of the source
+// code based on fuzzing.
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/project-oak/transparent-release/pkg/amber"
+)
+
+// FuzzClaimV1 is the URI that should be used as the ClaimType in V1 Amber
+// Claim representing a V1 Fuzz Claim.
+const FuzzClaimV1 = "https://github.com/project-oak/transparent-release/fuzz_claim/v1"
+
+// FuzzClaimSpec gives the `ClaimSpec` definition. It will be included in a
+// Claim, which itself is part of an in-toto statement where the subject
+// refers to a Git repository.
+type FuzzClaimSpec struct {
+	// `ClaimSpec` per fuzz-target.
+	PerTarget []FuzzSpecPerTarget `json:"perTarget"`
+	// `ClaimSpec` for all fuzz-targets.
+	PerProject *FuzzStats `json:"perProject"`
+}
+
+// FuzzSpecPerTarget contains the fuzzing claims specification per fuzz-target.
+type FuzzSpecPerTarget struct {
+	// Name of the fuzz-target.
+	Name string `json:"name"`
+	// Path of the fuzz-target, relative to the root of the Git repository.
+	Path string `json:"path"`
+	// Fuzzing statistics of the fuzz-target.
+	FuzzStats *FuzzStats `json:"fuzzStats"`
+}
+
+// FuzzStats contains the fuzzing statistics of the revision
+// of the source code for all fuzz-targets or a fuzz-target.
+type FuzzStats struct {
+	// Coverage specifies the code coverage by all fuzz-targets or a fuzz-target.
+	Coverage *FuzzCoverage `json:"coverage"`
+	// Bugs specifies the  number of detected bugs using all fuzz-targets
+	// or a fuzz-target.
+	DetectedBugs int `json:"detectedBugs"`
+	// Crashes specifies the total number of crashes that happened when running
+	// the fuzz-targets or the number of fuzzer runs that crashed for a fuzz-target.
+	Crashes int `json:"crashes"`
+	// FuzzEffort specifies the fuzzing efforts spent while using all fuzz-targets
+	// or on running a fuzz-target.
+	FuzzEffort *FuzzEffortSpec `json:"fuzzEffort,omitempty"`
+}
+
+// FuzzCoverage contains the code coverage by fuzz testing.
+type FuzzCoverage struct {
+	// Line specifies line coverage.
+	LineCoverage string `json:"lineCoverage"`
+	// Branch specifies branch coverage.
+	BranchCoverage string `json:"branchCoverage"`
+}
+
+// FuzzEffortSpec contains the fuzzing efforts.
+type FuzzEffortSpec struct {
+	// FuzzTimeSeconds specifies the fuzzing time in seconds.
+	FuzzTimeSeconds int `json:"fuzzTimeSeconds,omitempty"`
+	// NumberTests specifies the number of executed fuzzing tests.
+	NumberTests int `json:"numberTests,omitempty"`
+}
+
+// ValidateFuzzClaim validates that an Amber Claim is a Fuzz Claim with a valid ClaimType.
+// If valid, the ClaimPredicate object is returned. Otherwise an error is returned.
+func ValidateFuzzClaim(claimPredicate amber.ClaimPredicate) (*amber.ClaimPredicate, error) {
+	if claimPredicate.ClaimType != FuzzClaimV1 {
+		return nil, fmt.Errorf(
+			"the claimPredicate does not have the expected claim type; got: %s, want: %s",
+			claimPredicate.ClaimType,
+			FuzzClaimV1)
+	}
+
+	// Verify the type of the ClaimSpec, and return it if it is of type ClaimPredicate.
+	switch claimPredicate.ClaimSpec.(type) {
+	case FuzzClaimSpec:
+		return validateFuzzClaimSpec(claimPredicate)
+	default:
+		return nil, fmt.Errorf(
+			"the claimSpec does not have the expected type; got: %T, want: FuzzClaimSpec",
+			claimPredicate.ClaimSpec)
+	}
+}
+
+// validateFuzzClaimSpec validates details about the FuzzClaimSpec.
+func validateFuzzClaimSpec(claimPredicate amber.ClaimPredicate) (*amber.ClaimPredicate, error) {
+	log.Println("Not yet implemented!")
+	return &claimPredicate, nil
+}
