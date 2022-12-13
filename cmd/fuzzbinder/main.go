5a774062148e40472fdc2f0a38367a5ecdfb81a9
Marah Gamdou <mgamdou@google.com>
2022-12-13 16:42:26 +0000

    Add FuzzBinder cmd

--- cmd/fuzzbinder/main.go
+++ cmd/fuzzbinder/main.go
@@ -17,9 +17,54 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/project-oak/transparent-release/internal/fuzzbinder"
 )
 
 func main() {
-	log.Println("Not yet implemented!")
+	fuzzParameters := &fuzzbinder.FuzzParameters{}
+	var fuzzClaimPath string
+	var date string
+	flag.StringVar(&fuzzParameters.ProjectName, "project_name", "",
+		"Required - Project name as defined in OSS-Fuzz projects.")
+	flag.StringVar(&fuzzParameters.ProjectHomepage, "homepage", "",
+		"Required - Project homepage.")
+	flag.StringVar(&fuzzParameters.FuzzEngine, "fuzzengine", "",
+		"Required - Fuzzing engine.")
+	flag.StringVar(&fuzzParameters.Sanitizer, "sanitizer", "",
+		"Required - Fuzzing sanitizer.")
+	flag.StringVar(&date, "date", "",
+		"Required - Fuzzing date.")
+	flag.StringVar(&fuzzClaimPath, "fuzzclaim_path", "",
+		"Required - Output file path for storing the generated fuzzing claim.")
+	flag.Parse()
+
+	// Get the absolute path for storing the fuzzing the claim.
+	absFuzzClaimPath, err := filepath.Abs(fuzzClaimPath)
+	if err != nil {
+		log.Fatalf("Couldn't get absolute path for storing the fuzzing claim: %v", err)
+	}
+
+	// Generate the fuzzing claim.
+	statement, err := fuzzbinder.GenerateFuzzClaim(date, fuzzParameters)
+	if err != nil {
+		log.Fatalf("Couldn't generate the fuzzing claim: %v", err)
+	}
+
+	// Write the fuzzing claim to file and apply indent to it.
+	bytes, err := json.MarshalIndent(statement, "", "    ")
+	if err != nil {
+		log.Fatalf("Couldn't marshal the fuzzing claim: %v", err)
+	}
+
+	// Store the fuzzing claim.
+	log.Printf("Storing the fuzzing claim in %s", absFuzzClaimPath)
+	if err := os.WriteFile(absFuzzClaimPath, bytes, 0600); err != nil {
+		log.Fatalf("Couldn't write the fuzzing claim file: %v", err)
+	}
 }
