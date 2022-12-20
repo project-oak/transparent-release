da12b4ec5ff26d399366e60cb6ff0b6a7e376cae
Marah Gamdou <mgamdou@google.com>
2022-12-20 15:20:36 +0000

    Update error messages

--- internal/fuzzbinder/util_test.go
+++ internal/fuzzbinder/util_test.go
@@ -14,31 +14,53 @@
 package fuzzbinder
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 	"time"
 )
 
 func TestValidateFuzzingDate(t *testing.T) {
-	currentTimeStr := "2022-12-20 14:06:49.055696838 +0000 UTC"
+	referenceTimeStr := "2022-12-20 14:06:49.055696838 +0000 UTC"
 	layout := "2006-01-02 15:04:05 -0700 MST"
-	currentTime, err := time.Parse(layout, currentTimeStr)
+	referenceTime, err := time.Parse(layout, referenceTimeStr)
 	if err != nil {
 		t.Fatalf("could not parse current time: %v", err)
 	}
+
 	validDates := []string{"20221206", "20221207", "20221208", "20221215",
 		"20221219", "20221220"}
-	invalidDates := []string{"20221321", "2023122", "20231221", "20221221",
-		"20221222", "20221223", "20221205", "20221204", "20221121"}
 	for _, date := range validDates {
-		err := ValidateFuzzingDate(date, currentTime)
+		err := ValidateFuzzingDate(date, referenceTime)
 		if err != nil {
-			t.Errorf("unexpected fuzzing date validation error : got %v want nil", err)
+			t.Errorf("unexpected fuzzing date validation error : got %v want %v", err, nil)
 		}
 	}
-	for _, date := range invalidDates {
-		err := ValidateFuzzingDate(date, currentTime)
-		if err == nil {
-			t.Errorf("unexpected fuzzing date validation error : got nil want validation error")
+
+	invalidFormatDates := []string{"20221321", "2023122"}
+	for _, date := range invalidFormatDates {
+		want := fmt.Sprintf("the format of %s is not valid: the date format should be yyyymmdd", date)
+		err := ValidateFuzzingDate(date, referenceTime)
+		if err == nil || !strings.Contains(err.Error(), want) {
+			t.Errorf("unexpected fuzzing date validation error : got %v want %v", err, want)
+		}
+	}
+
+	invalidPastDates := []string{"20221205", "20221204", "20221121"}
+	for _, date := range invalidPastDates {
+		want := fmt.Sprintf("the fuzzing logs on %s are deleted: select a more recent date", date)
+		err := ValidateFuzzingDate(date, referenceTime)
+		if err == nil || !strings.Contains(err.Error(), want) {
+			t.Errorf("unexpected fuzzing date validation error : got %v want %v", err, want)
+		}
+	}
+
+	invalidFutureDates := []string{"20231221", "20221221", "20221222", "20221223"}
+	for _, date := range invalidFutureDates {
+		want := fmt.Sprintf("no fuzzing logs generated for %s: do not select a date in the future", date)
+		err := ValidateFuzzingDate(date, referenceTime)
+		if err == nil || !strings.Contains(err.Error(), want) {
+			t.Errorf("unexpected fuzzing date validation error : got %v want %v", err, want)
 		}
 	}
 }
