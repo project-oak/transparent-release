be64fa663a4e1b52f632d3a653dbc0cc54a7eaef
rbehjati <razieh@google.com>
2022-12-29 13:25:31 +0000

    Add tests for FromProvenance (#203)
    * Add positive test for FromProvenance with Amber
    * Add negative test for FromProvenance with SLSAv1

--- internal/common/common_test.go
+++ internal/common/common_test.go
@@ -16,17 +16,21 @@ package common
 
 import (
 	"fmt"
+	"os"
 	"path/filepath"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/project-oak/transparent-release/internal/testutil"
 	"github.com/project-oak/transparent-release/pkg/amber"
 	slsa "github.com/project-oak/transparent-release/pkg/intoto/slsa_provenance/v0.2"
+	"github.com/project-oak/transparent-release/pkg/types"
 )
 
 const (
 	testdataPath             = "../../testdata/"
 	provenanceExamplePath    = "provenance.json"
+	slsav1ProvenancePath     = "slsa_v1_provenance.json"
 	wantTOMLDigest           = "322527c0260e25f0e9a2595bd0d71a52294fe2397a7af76165190fd98de8920d"
 	wantBuilderImageID       = "6e5beabe4ace0e3aaa01ce497f5f1ef30fed7c18c596f35621751176b1ab583d"
 	wantSHA1HexDigitLength   = 40
@@ -143,6 +147,50 @@ func TestParseReferenceValues(t *testing.T) {
 	testutil.AssertEq(t, "binary digests[0]", referenceValues.BinarySHA256Digests[0], "322527c0260e25f0e9a2595bd0d71a52294fe2397a7af76165190fd98de8920d")
 	testutil.AssertEq(t, "want build cmd", referenceValues.WantBuildCmds, true)
 	testutil.AssertEq(t, "builder image digests[0]", referenceValues.BuilderImageSHA256Digests[0], "9e2ba52487d945504d250de186cb4fe2e3ba023ed2921dd6ac8b97ed43e76af9")
+}
+
+func TestFromProvenance_Amber(t *testing.T) {
+	path := filepath.Join(testdataPath, provenanceExamplePath)
+	provenance, err := amber.ParseProvenanceFile(path)
+	if err != nil {
+		t.Fatalf("couldn't parse the provenance file: %v", err)
+	}
+
+	want := NewProvenanceIR("322527c0260e25f0e9a2595bd0d71a52294fe2397a7af76165190fd98de8920d",
+		WithBuildType("https://github.com/project-oak/transparent-release/schema/amber-slsa-buildtype/v1/provenance.json"),
+		WithBuildCmd([]string{"cp", "testdata/static.txt", "test.txt"}),
+		WithBuilderImageSHA256Digest("9e2ba52487d945504d250de186cb4fe2e3ba023ed2921dd6ac8b97ed43e76af9"),
+		WithRepoURIs([]string{"https://github.com/project-oak/transparent-release"}))
+
+	got, err := FromProvenance(provenance)
+	if err != nil {
+		t.Fatalf("couldn't map provenance to ProvenanceIR: %v", err)
+	}
+
+	if diff := cmp.Diff(got, want, cmp.AllowUnexported(ProvenanceIR{})); diff != "" {
+		t.Errorf("unexpected provenanceIR: %s", diff)
+	}
+}
+
+func TestFromProvenance_Slsav1(t *testing.T) {
+	path := filepath.Join(testdataPath, slsav1ProvenancePath)
+	statementBytes, err := os.ReadFile(path)
+	if err != nil {
+		t.Fatalf("could not read the provenance file: %v", err)
+	}
+	provenance, err := types.ParseStatementData(statementBytes)
+	if err != nil {
+		t.Fatalf("couldn't parse the provenance file: %v", err)
+	}
+
+	// Currently SLSA v1.0 provenances are not supported, so we expect an error.
+	want := fmt.Sprintf("unsupported predicateType (%q) for provenance", "https://slsa.dev/provenance/v1.0")
+	_, err = FromProvenance(provenance)
+	got := fmt.Sprintf("%v", err)
+
+	if got != want {
+		t.Fatalf("got error %q, want error %q", got, want)
+	}
 }
 
 func checkBuildConfig(got *BuildConfig, t *testing.T) {
