73ad4edc941850dc7a5a44591f066fce7a8b0852
Razieh Behjati <razieh@google.com>
2023-04-20 18:23:21 +0100

    Remove BuildConfig and its related functionality

--- internal/common/common_test.go
+++ internal/common/common_test.go
@@ -27,14 +27,10 @@ import (
 )
 
 const (
-	testdataPath             = "../../testdata/"
-	provenanceExamplePath    = "amber_provenance.json"
-	slsav02ProvenancePath    = "slsa_v02_provenance.json"
-	slsav1ProvenancePath     = "slsa_v1_provenance.json"
-	wantTOMLDigest           = "322527c0260e25f0e9a2595bd0d71a52294fe2397a7af76165190fd98de8920d"
-	wantBuilderImageID       = "6e5beabe4ace0e3aaa01ce497f5f1ef30fed7c18c596f35621751176b1ab583d"
-	wantSHA1HexDigitLength   = 40
-	wantSHA256HexDigitLength = 64
+	testdataPath          = "../../testdata/"
+	slsav02ProvenancePath = "slsa_v02_provenance.json"
+	slsav1ProvenancePath  = "slsa_v1_provenance.json"
+	wantTOMLDigest        = "322527c0260e25f0e9a2595bd0d71a52294fe2397a7af76165190fd98de8920d"
 )
 
 func TestComputeBinarySHA256Digest(t *testing.T) {
@@ -45,50 +41,6 @@ func TestComputeBinarySHA256Digest(t *testing.T) {
 	}
 	if got != wantTOMLDigest {
 		t.Errorf("invalid SHA256 digest: got %s, want %s", got, wantTOMLDigest)
-	}
-}
-
-func TestLoadBuildConfigFromFile(t *testing.T) {
-	path := filepath.Join(testdataPath, "build.toml")
-	config, err := LoadBuildConfigFromFile(path)
-	if err != nil {
-		t.Fatalf("couldn't load build file: %v", err)
-	}
-	checkBuildConfig(config, t)
-}
-
-func TestParseBuilderImageURIValidURI(t *testing.T) {
-	builderImageURI := fmt.Sprintf("gcr.io/oak-ci/oak@sha256:%s", wantBuilderImageID)
-	alg, digest, err := parseBuilderImageURI(builderImageURI)
-	if err != nil {
-		t.Fatalf("couldn't parse imageURI (%q): %v", builderImageURI, err)
-	}
-
-	if alg != "sha256" {
-		t.Errorf("got parseBuilderImageURI(%s).algorithm = %s, want sha256", builderImageURI, alg)
-	}
-
-	if digest != wantBuilderImageID {
-		t.Errorf("got parseBuilderImageURI(%s).digest = %s, want %s",
-			builderImageURI, alg, wantBuilderImageID)
-	}
-}
-
-func TestParseBuilderImageURIInvalidURIs(t *testing.T) {
-	imageURIWithTag := "gcr.io/oak-ci/oak@latest"
-	want := fmt.Sprintf("the builder image digest (%q) does not have the required ALG:VALUE format", "latest")
-	alg, digest, err := parseBuilderImageURI(imageURIWithTag)
-	got := fmt.Sprintf("%v", err)
-	if got != want {
-		t.Fatalf("got (%s, %s, %v) = parseBuilderImageURI(%q), want (_, _, %s)", alg, digest, err, imageURIWithTag, want)
-	}
-
-	invalidURI := "gcr.io/oak-ci/oak"
-	want = fmt.Sprintf("the builder image URI (%q) does not have the required NAME@DIGEST format", invalidURI)
-	alg, digest, err = parseBuilderImageURI(invalidURI)
-	got = fmt.Sprintf("%v", err)
-	if got != want {
-		t.Fatalf("got (%s, %s, %v) = parseBuilderImageURI(%q), want (_, _, %s)", alg, digest, err, invalidURI, want)
 	}
 }
 
@@ -150,19 +102,4 @@ func TestFromProvenance_Slsav1(t *testing.T) {
 	if got != want {
 		t.Fatalf("got error %q, want error %q", got, want)
 	}
-}
-
-func checkBuildConfig(got *BuildConfig, t *testing.T) {
-	alg, digest, err := parseBuilderImageURI(got.BuilderImage)
-	if err != nil {
-		t.Fatalf("couldn't parse imageURI (%q): %v", got.BuilderImage, err)
-	}
-	// Check that the provenance is generated correctly
-	testutil.AssertEq(t, "repoURL", got.Repo, "https://github.com/project-oak/transparent-release")
-	testutil.AssertEq(t, "commitHash length", len(got.CommitHash), wantSHA1HexDigitLength)
-	testutil.AssertEq(t, "builderImageID length", len(digest), wantSHA256HexDigitLength)
-	testutil.AssertEq(t, "builderImageID digest algorithm", alg, "sha256")
-	testutil.AssertEq(t, "builderImageID length", len(digest), wantSHA256HexDigitLength)
-	testutil.AssertNonEmpty(t, "command[0]", got.Command[0])
-	testutil.AssertNonEmpty(t, "command[1]", got.Command[1])
 }
