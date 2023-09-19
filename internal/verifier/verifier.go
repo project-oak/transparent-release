9ab42855a4efdad6268791ee25d01ec1b56ed006
Tom Binder <tbinder@google.com>
2023-09-19 15:50:32 +0200

    Fix linter issues.

--- internal/verifier/verifier.go
+++ internal/verifier/verifier.go
@@ -27,8 +27,7 @@ import (
 // Verify checks that the provenance conforms to expectations, returning a
 // list of errors whenever the verification failed.
 //
-//nolint:cyclop
-//nolint:gocognit
+//nolint:cyclop,gocognit
 func Verify(provenances []model.ProvenanceIR, verOpts *pb.VerificationOptions) error {
 	if provenances == nil {
 		panic(fmt.Errorf("provenances must not be nil"))
